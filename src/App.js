eb9bf65f0cb82c57ab7d7af6e6a326847ab644b8
brunozn <brunojndias@gmail.com>
2020-06-26 11:06:33 -0300

    Finalizando o fileList estatico

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { uniqueId } from 'lodash';
+import filesize from 'filesize';
 
 import GlobalStyle from './styles/global.js';
 
@@ -7,19 +9,47 @@ import {Containe , Content} from './styles';
 import Upload from "./components/Upload";
 import FileList from "./components/FileList";
 
-function App() {
-  //const { uploadedFiles } = '';
+class App extends Component{
+  state = {
+    UploadedFiles: [],
+  };
+
+  handleUpload = files => {
+    console.log(files)
+    const UploadedFiles = files.map(file => ({
+      file, 
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      Uploaded: false,
+      error: false,
+      url: null,
+    }))
+
+    this.setState({
+      UploadedFiles: this.state.UploadedFiles.concat(UploadedFiles)
+    });
+  };
+
+  
+  render() {
+    const { UploadedFiles} = this.state;
   return (
     <Containe> 
        <GlobalStyle />
       <Content>
-      <Upload />
-      <FileList />
+      <Upload onUpload={this.handleUpload} />
+      { !!UploadedFiles.length  && (
+          <FileList files={UploadedFiles} />
+      )}
+      
 
       </Content>
     </Containe>
    
   );
 }
-
+}
 export default App;
--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -6,45 +6,49 @@ import {MdLink, MdCheckCircle, MdError} from 'react-icons/md';
 
 import { Contain, FileInfo,Preview } from "./styles";
 
-const FileList = () => (
+const FileList = ({ files }) => (
   <Contain>
-    <li>
-        <FileInfo>
-          <Preview src="https://images.unsplash.com/photo-1531170515419-b26209bbe710?ixlib=rb-1.2.1&auto=format&fit=crop&w=1375&q=80"/>
-          <div>
-            <strong>imag.png</strong>
-            <span> 64kb
-                <button onClick={() => {}}>
-                  Excluir
-                </button>
-              
-            </span>
-          </div>
-        </FileInfo>
+      {files.map(uploadedFile => (
+            <li>
+            <FileInfo>
+              <Preview src={uploadedFile.preview} />
+              <div>
+                <strong>{uploadedFile.name}</strong>
+                <span> {uploadedFile.readableSize}{" "}
+                  { !!uploadedFile.url &&(
+                    <button onClick={() => {}}>Excluir</button>
+                  )}
+                </span>
+              </div>
+            </FileInfo>
+    
+            <div>
+              {!uploadedFile.uploaded && !uploadedFile.error && (
+                <CircularProgressbar
+                styles= {{
+                  root: { width: 24},
+                  path: { stroke: '#005166'}
+                }}
+                strokeWidth={10}
+                percentage={uploadedFile.progress}
+                />
+              )}
+                {uploadedFile.url && (
+                <a
+                  href="https://images.unsplash.com/photo-1531170515419-b26209bbe710?ixlib=rb-1.2.1&auto=format&fit=crop&w=1375&q=80"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
+                    <MdLink style={{marginRight: 8}} size={24} color="222" />
+                </a>
+                 )}
 
-        <div>
-          <CircularProgressbar
-            styles= {{
-              root: { width: 24},
-              path: { stroke: '#005166'}
-            }}
-            strokeWidth={10}
-            percentage={60}
-            />
+                  { uploadedFile && <MdCheckCircle size={24} color="#78e5d5" /> }
+                  { uploadedFile.error && <MdError size={24} color="#e57878" /> }
+            </div>
+          </li>
 
-            <a
-              href="https://images.unsplash.com/photo-1531170515419-b26209bbe710?ixlib=rb-1.2.1&auto=format&fit=crop&w=1375&q=80"
-              target="_blank"
-              rel="noopener noreferrer"
-              >
-                <MdLink style={{marginRight: 8}} size={24} color="222" />
-            </a>
-
-            <MdCheckCircle/>
-            <MdError/>
-        </div>
-
-      </li>
+      ))}
   </Contain>
 );
 
